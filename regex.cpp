1f5662fd4c3e15a4912aedd73705bed8dfc51ab5
vivek <vivekroy2584@gmail.com>
2025-04-23 20:19:42 +0000

    fragment combining and NFA builder implemented

--- regex.cpp
+++ regex.cpp
@@ -59,10 +59,94 @@ std::string regex_to_postfix(const std::string& regex){
 }
 
 State* postfix_to_nfa(const std::string& postfix_expr){
-    std::stack<Fragment> fragment_stack;
+    std::stack<Fragment*> fragment_stack;
+    Fragment *frag, *frag1, *frag2;
     for(const char& c : postfix_expr){
         switch(c){
-            
+
+            case '.' :                                             // CONCATENATION
+                frag1 = fragment_stack.top();
+                fragment_stack.pop();
+                frag2 = fragment_stack.top();
+                fragment_stack.pop();
+                patch(frag1 -> out_list, frag2 -> start_state);
+                Fragment* new_fragment = new Fragment(frag1 -> start_state, frag2 -> out_list);
+                fragment_stack.push(new_fragment);
+                break;
+
+            case '|':                                             // ALTERNATION
+                frag1 = fragment_stack.top();
+                fragment_stack.pop();
+                frag2 = fragment_stack.top();
+                fragment_stack.pop();
+                State* new_state = new State(SPLIT, frag1 -> start_state, frag2 -> start_state);
+                Ptrlist* new_list = append(frag1 -> out_list, frag2 -> out_list);
+                Fragment* new_fragment = new Fragment(new_state, new_list);
+                fragment_stack.push(new_fragment);
+                break;
+
+            case '?':                                             //EXISTENCE
+                frag = fragment_stack.top();
+                fragment_stack.pop();
+                State* new_state = new State(SPLIT, frag -> start_state, nullptr);
+                Ptrlist* new_list = append(frag -> out_list, make_ptrlist(&(new_state -> out1)));
+                Fragment* new_fragment = new Fragment(new_state, new_list);
+                fragment_stack.push(new_fragment);
+                break;
+
+            case '*':                                             // KLENEE STAR
+                frag = fragment_stack.top();
+                fragment_stack.pop();
+                State* new_state = new State(SPLIT, frag -> start_state, nullptr);
+                patch(frag -> out_list, new_state);
+                Fragment* new_fragment = new Fragment(new_state, make_ptrlist(&(new_state -> out1)));
+                fragment_stack.push(new_fragment);
+                break;
+
+            case '+':                                             //ONE OR MORE EXISTENCE
+                frag = fragment_stack.top();
+                fragment_stack.pop();
+                State* new_state = new State(SPLIT, frag -> start_state, nullptr);
+                patch(frag -> out_list, new_state);
+                Fragment* new_fragment = new Fragment(frag -> start_state, make_ptrlist(&(new_state -> out1)));
+                fragment_stack.push(new_fragment);
+                break;
+
+            default:
+                State* new_state = new State(c, nullptr, nullptr);
+                Fragment* new_fragment = new Fragment(new_state, make_ptrlist(&(new_state -> out1)));
+                fragment_stack.push(new_fragment);
+                break;
         }
+    }
+
+    frag = fragment_stack.top();
+    fragment_stack.pop();
+    State* matchstate = new State(MATCH, nullptr, nullptr);
+    patch(frag -> out_list, matchstate);
+    State* start = frag -> start_state;
+    return start;  
+}
+
+Ptrlist* make_ptrlist(State** outptr){
+    Ptrlist* new_ptr_list = new Ptrlist;
+    new_ptr_list -> outptr = outptr;
+    return new_ptr_list;
+}
+
+Ptrlist* append(Ptrlist* l1, Ptrlist* l2){
+    if(!l1) return l2;
+    Ptrlist* head = l1;
+    while(l1 -> next != nullptr){
+        l1 = l1 -> next;
+    }
+    l1 -> next = l2;
+    return head;
+}
+
+void patch(Ptrlist* list, State* s){
+    while(list -> next != nullptr){
+        *(list -> outptr) = s;
+        list = list -> next;
     }
 }
--- regex.hpp
+++ regex.hpp
@@ -6,15 +6,27 @@
 #include <unordered_map>
 #include <stack>
 
+enum state_type{
+    NORMAL,
+    SPLIT,
+    MATCH
+};
+
 std::string regex_to_postfix(const std::string& regex);
 
 struct State{
     //public:
         char transition_character;
+        state_type state_name;
         State* out1;
         State* out2;
 
-        State(char transition_character) : transition_character(transition_character), out1(nullptr), out2(nullptr){};
+        State()                                                    : transition_character('\0'), state_name(NORMAL), 
+                                                                     out1(nullptr), out2(nullptr){};
+        State(state_type state_name, State* out1, State* out2)     : transition_character('\0'), state_name(state_name), 
+                                                                     out1(out1), out2(out2){};
+        State(char transition_character, State* out1, State* out2) : transition_character(transition_character), 
+                                                                     state_name(NORMAL), out1(out1), out2(out2){};                                                                                         
 };
 
 struct Ptrlist{
@@ -24,9 +36,10 @@ struct Ptrlist{
 
     //public:
         Ptrlist() : outptr(nullptr), next(nullptr){};
-        Ptrlist* make_ptrlist(State** outptr);
-        Ptrlist* append(Ptrlist* l1, Ptrlist* l2);
 };
+
+Ptrlist* make_ptrlist(State** outptr);
+Ptrlist* append(Ptrlist* l1, Ptrlist* l2);
 
 struct Fragment{
     //public:
@@ -34,6 +47,7 @@ struct Fragment{
         Ptrlist* out_list;
 
         Fragment() : start_state(nullptr), out_list(nullptr){};
+        Fragment(State* start_state, Ptrlist* out_list) : start_state(start_state), out_list(out_list){};
 };
 
 void patch(Ptrlist* list, State* s);
